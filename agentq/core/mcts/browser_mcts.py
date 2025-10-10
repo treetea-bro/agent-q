4bff3cf308a269751d07b3a456c545cf22919192
peco <sorpwjdafh@naver.com>
2025-10-10 15:27:51 +0900

    fix(hi): hi

--- agentq/core/mcts/browser_mcts.py
+++ agentq/core/mcts/browser_mcts.py
@@ -11,6 +11,7 @@ import numpy as np
 import torch
 from datasets import Dataset
 from langsmith import traceable
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from playwright.async_api import Page
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from trl.trainer.dpo_config import DPOConfig
